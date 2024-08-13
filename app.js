8c3119bcb4f8e28891b12037f62bd6bdb50d617e
Abhinay Singh <abhinaysingh0707@gmail.com>
2024-08-13 17:58:25 +0530

    write js for the to-do list

--- app.js
+++ app.js
@@ -9,5 +9,38 @@ function addTask(){
         let li = document.createElement("li");
         li.innerHTML = inpBox.value;
         listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
+
+    inpBox.value = "";
+    saveData();
 }
+
+inpBox.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("myBtn").click();
+    saveData();
+  }
+});
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false); 
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
